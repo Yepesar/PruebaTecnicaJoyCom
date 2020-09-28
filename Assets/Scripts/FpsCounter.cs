6615de41eeb71e33a0a6237a3c031528b01a03af
LeJose <jdyepes@hotmail.com>
2020-09-28 00:42:32 -0500

    Final postproccesing changes

---
+++ Assets/Scripts/FpsCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FpsCounter : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI fpsTexts;
+
+    int currentFPS;
+
+    void Update()
+    {
+        currentFPS = (int)(1f / Time.unscaledDeltaTime);
+        
+    }
+
+    private void LateUpdate()
+    {
+        fpsTexts.text = "FPS: " + currentFPS;
+    }
+}
